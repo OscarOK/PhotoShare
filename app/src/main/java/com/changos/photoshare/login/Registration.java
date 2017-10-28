93ecfc5bcb6c1a192bf97558293b63d0e2b89ff3
Alejandro Romero <a310938@uach.mx>
2017-10-28 03:25:31 -0600

    Registartion Terminado

--- app/src/main/java/com/changos/photoshare/login/Registration.java
+++ app/src/main/java/com/changos/photoshare/login/Registration.java
@@ -1,13 +1,17 @@
 package com.changos.photoshare.login;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.changos.photoshare.MainActivity;
 import com.changos.photoshare.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -39,8 +43,8 @@ public class Registration extends AppCompatActivity {
                 String email = emailEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
                 String confirmPassword = confirmPasswordEditText.getText().toString();
-
-                if(password == confirmPassword && password != null){
+                Log.d("jojito", password + " " + confirmPassword);
+                if(password.equals(confirmPassword) && !TextUtils.isEmpty(password) ){
                     registerNewUser(email, password);
                 }else Toast.makeText(Registration.this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
 
@@ -53,7 +57,12 @@ public class Registration extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
-
+                    Intent i = new Intent();
+                    i.setClass(Registration.this, MainActivity.class);
+                    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(i);
+                }else{
+                    Toast.makeText(Registration.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
                 }
             }
         });
