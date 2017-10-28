16211a151ce8a4d6b19fc6fe9432587d745d1512
Alejandro Romero <a310938@uach.mx>
2017-10-28 00:57:24 -0600

    Update Login

--- app/src/main/java/com/changos/photoshare/login/Login.java
+++ app/src/main/java/com/changos/photoshare/login/Login.java
@@ -2,6 +2,8 @@ package com.changos.photoshare.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 import com.changos.photoshare.R;
 
@@ -11,5 +13,9 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Texto registrar en negritas
+        TextView textView = (TextView) findViewById(R.id.registerTextBtn);
+        textView.setText(Html.fromHtml("No tienes cuenta? <b>Registrate.</b>"));
     }
 }
