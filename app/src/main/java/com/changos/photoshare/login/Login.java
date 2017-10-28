e9cc5e2f35fb9ce3708ec85e762e2293254c86a1
Alejandro Romero <a310938@uach.mx>
2017-10-28 00:39:02 -0600

    Login

--- app/src/main/java/com/changos/photoshare/login/Login.java
+++ app/src/main/java/com/changos/photoshare/login/Login.java
@@ -3,6 +3,8 @@ package com.changos.photoshare.login;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.changos.photoshare.R;
+
 public class Login extends AppCompatActivity {
 
     @Override
